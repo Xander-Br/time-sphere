d50c41c69f99fce27fad7515c562a95df66b2abc
Alexandre Breuil <breuilalexandre2003@gmail.com>
2023-04-01 14:54:17 +0200

    auth

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 import time
 from datetime import datetime
-from http.client import HTTPException
-from fastapi import FastAPI, Query, File, UploadFile, Form
+
+
+import bcrypt
+from fastapi import FastAPI, Query, File, UploadFile, Form, HTTPException
 from pydantic import BaseModel, Field
 from typing import Optional
 from pymongo import MongoClient
+from starlette import status
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
 
@@ -13,20 +16,43 @@ app = FastAPI()
 client = MongoClient(
     'mongodb+srv://doadmin:9c43u0Vd876E2SRH@db-mongodb-fra1-00555-517d35a7.mongo.ondigitalocean.com/admin?tls=true&authSource=admin')
 db = client["db"]
-collection = db["capsule"]
-collection.create_index([("location", "2dsphere")])
+capsuleCollection = db["capsule"]
+capsuleCollection.create_index([("location", "2dsphere")])
+
+userCollection = db["capsule"]
 
 STATIC_DIR = "static"
 os.makedirs(STATIC_DIR, exist_ok=True)
 
 
-class UserData(BaseModel):
+class User(BaseModel):
+    username: str
+    password: str
+
+
+
+
+class Capsule(BaseModel):
     username: str
     location: dict = Field(..., example={"type": "Point", "coordinates": [0.0, 0.0]})
     created_at: Optional[datetime] = datetime.now()
     unlock_at: Optional[datetime] = None
     data_img_src: Optional[str] = None
     data_text: str
+    friends: Optional[list]
+
+
+
+def authenticate_user(username: str, password: str):
+    user = userCollection.find_one({"username": username})
+    try:
+        if not user:
+            return None
+        if not password != user["password"]:
+            return None
+    except:
+        return None
+    return user
 
 
 app.add_middleware(
@@ -54,12 +80,12 @@ async def create_document(
         username: str = Form(...),
         location: str = Form(...),
         text: str = Form(...),
+        longitude: float = Form(...),
+        latitude: float = Form(...),
         unlock_at: datetime = Form(...),
         img: UploadFile = File(...),
 ):
-    import json
-
-    location_dict = json.loads(location)
+    location_dict = {"type": "Point", "coordinates": [longitude, latitude]}
 
     img_path, filename = await save_image(img)
     img_url = f"/static/{filename}"
@@ -73,7 +99,7 @@ async def create_document(
         "text": text,
     }
 
-    result = collection.insert_one(user_data)
+    result = capsuleCollection.insert_one(user_data)
     if result:
         return {"status": "success", "message": "Document created", "id": str(result.inserted_id)}
     else:
@@ -106,8 +132,23 @@ async def get_documents_near_location(longitude: float = Query(..., description=
     }
 
     documents = []
-    for doc in collection.find(query):
+    for doc in capsuleCollection.find(query):
         doc["_id"] = str(doc["_id"])
         documents.append(doc)
 
     return documents
+
+
+@app.post("/login")
+async def login_user(username: str = Form(...), password: str = Form(...)):
+    user = authenticate_user(username, password)
+    if not user:
+        user_data = {"username": username, "password": password}
+
+        result = userCollection.insert_one(user_data)
+        if result:
+            user = {"_id": str(result.inserted_id), "username": username}
+            return {"status": "success", "message": "User created and authenticated", "user": user}
+
+        user = {"_id": str(user["_id"]), "username": user["username"]}
+        return {"status": "success", "message": "User authenticated", "user": user}
